b70797fbd7a98b7afc48701a94c2aed5b10cd8de
seeungKang <seeung0305@naver.com>
2019-05-12 08:41:28 +0900

    working on ui in review

--- src/components/info/MenuReview/MenuReview.js
+++ src/components/info/MenuReview/MenuReview.js
@@ -5,11 +5,31 @@ import sendIcon from 'static/2/2-3/2_3_send_ui.png';
 const MenuReview = () => {
   return (
     <div className="review-area">
-        <div className="footer">
-        <input type="text" id="comment-area" placeholder="Enter your comments"/>
-        <img id="sendIcon" src={sendIcon} alt=""/>
-        </div>
+
+    <div className="rest-area">
+      <div className="box">
+        <div className="nametag">Anonymous1</div>
+        <div className="comment">loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDo</div>
+      </div>
+      <div className="box">
+        <div className="nametag">Anonymous2</div>
+        <div className="comment">loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDo</div>
+      </div>
+      <div className="box">
+        <div className="nametag">Anonymous3</div>
+        <div className="comment">loremIpsumDolorSitAmetConsecteturAdipiscingElitSedDo</div>
+      </div>
     </div>
+
+
+    <div className="footer">
+      <input type="text" id="comment-area" placeholder="Enter your comments"/>
+      <img id="sendIcon" src={sendIcon} alt=""/>
+    </div>
+
+    </div>
+
+    
   );
 };
 
