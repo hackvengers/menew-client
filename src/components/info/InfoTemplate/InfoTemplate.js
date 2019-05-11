1c910a847ee5d5fdee62c07847c3f20d5b2b691a
zerone2 <yicho93@naver.com>
2019-05-12 04:56:04 +0900

    info menu detail update

--- src/components/info/InfoTemplate/InfoTemplate.js
+++ src/components/info/InfoTemplate/InfoTemplate.js
@@ -3,9 +3,22 @@ import './InfoTemplate.scss';
 import MenuName from 'components/info/MenuName';
 import InfoMenu from 'components/info/InfoMenu';
 import MenuPicture from 'components/info/MenuPicture';
+import MenuAbout from 'components/info/MenuAbout';
+import MenuReview from 'components/info/MenuReview';
+
 import exitIcon from 'static/2/2_common/2_X_icon.png';
 
 class InfoTemplate extends Component {
+  state = {
+    curTab: 'about'
+  };
+
+  showInfoMenu = () => {
+    if(this.state.curTab === 'image') return <MenuPicture/>;
+    if(this.state.curTab === 'about') return <MenuAbout/>;
+    if(this.state.curTab === 'review') return <MenuReview/>;
+  };
+
   render() {
     return (
       <div className="info-container">
@@ -14,7 +27,7 @@ class InfoTemplate extends Component {
         </div>
         <MenuName/>
         <InfoMenu/>
-        <MenuPicture/>
+        {this.showInfoMenu()}
       </div>
     );
   }
---
+++ src/components/info/MenuAbout/MenuAbout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MenuAbout.scss';
+
+const MenuAbout = () => {
+  return (
+    <div className="about-area">
+      alksadlksdaklasdklasdlkasd
+    </div>
+  );
+};
+
+export default MenuAbout;
