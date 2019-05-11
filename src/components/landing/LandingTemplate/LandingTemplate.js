861c22140d12aa29b9106aace98dabca65d086bd
zerone2 <yicho93@naver.com>
2019-05-11 21:31:25 +0900

    page1 ver1 complete

--- src/components/landing/LandingTemplate/LandingTemplate.js
+++ src/components/landing/LandingTemplate/LandingTemplate.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import './LandingTemplate.scss';
 import logo from 'static/1-1/1_1_logo.png';
-import phrase1 from 'static/1-3/1_3_text.png';
-import next_icon from 'static/1-1/1_1_next_icon.png';
-import skip_icon from 'static/1-1/1_1_skip.png';
+import phrase1 from 'static/1-1/1_1_text.png';
+import next_icon from 'static/1_common/1_common_next_icon.png';
+import skip_icon from 'static/1_common/1_common_skip.png';
 import progress_icon from 'static/1-1/1_1_circle.png';
 
 class LandingTemplate extends Component {
 
+  state = {
+    fadeOut: false
+  };
+
+  toggle = () => {
+    this.setState({
+      fadeOut: !this.state.fadeOut
+    })
+  };
+
   render() {
     return (
-      <div className="landing">
+      <div className={this.state.fadeOut ? "landing active" : "landing"}>
         <img id="main_logo" src={logo} alt=""/>
         <div id="main_phrase">
           <img className="phrase1" src={phrase1} alt=""/>
-          <img className="next_icon" src={next_icon} alt=""/>
+          <img className="next_icon" src={next_icon} alt="" onClick={this.toggle}/>
         </div>
         <div className="footer">
           <img className="progress_icon" src={progress_icon} alt=""/>
