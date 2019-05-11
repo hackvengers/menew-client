80000350e08490c047220cf5d7b254b462671008
zerone2 <yicho93@naver.com>
2019-05-11 15:13:25 +0900

    architecture alpha version complete

---
+++ src/components/landing/LandingTemplate/LandingTemplate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './LandingTemplate.scss';
+
+class LandingTemplate extends Component {
+  render() {
+    return (
+      <div className="landing">
+      </div>
+    )
+  }
+}
+
+export default LandingTemplate;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LandingTemplate from "../components/landing/LandingTemplate/LandingTemplate";
+
+const Home = () => {
+  return (
+    <LandingTemplate/>
+  );
+};
+
+export default Home;
